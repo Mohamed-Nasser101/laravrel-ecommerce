877edddf2bde62af902aa8c2c5ee17bee79161cb
Mohamed Nasser <mohamed.nasser3010@gmail.com>
2021-02-15 07:22:28 +0200

    check something

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
+
+    public function __construct(){
+        //$this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
