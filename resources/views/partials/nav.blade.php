c1196a61ffebbc5ff8e64cc7cd1acf5bae8bec85
Mohamed Nasser <mohamed.nasser3010@gmail.com>
2021-02-16 10:07:07 +0200

    start over

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,7 +2,13 @@
     <div class="top-nav container">
         <div class="logo"><a href="/">Laravel Ecommerce</a></div>
         @if (! request()->is('checkout'))
-        {{ menu('main','partials.menus.main') }}
+        {{-- {{ menu('main','partials.menus.main') }} --}}
+        <ul>
+            <li><a href="{{ route('shop.index') }}">Shop</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="{{ route('cart.index') }}">Cart <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span></a></li>
+        </ul>
         @endif
     </div> <!-- end top-nav -->
 </header>
